202afe77db1722afef419c3d5d389c54277c3ac7
M. Fadli Zein <m.fadli.zein.17@gmail.com>
2019-03-25 21:59:34 +0700

    Initial commit

---
+++ app/src/main/java/com/gzeinnumer/dragwithrecyclerview/DataNo.java
@@ -0,0 +1,13 @@
+package com.gzeinnumer.dragwithrecyclerview;
+
+class DataNo {
+    int no;
+
+    public DataNo(int no) {
+        this.no = no;
+    }
+
+    public int getNo() {
+        return no;
+    }
+}
